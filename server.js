69907b41d8d3fe07a4bfc884c671e6ed56d56f1d
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2022-03-18 13:51:54 +0100

    Connect db using mongoose + handle errors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to the database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -20,9 +25,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error: ${error.message}`.red);
+  // Close server and exit proccess
+  server.close(() => process.exit(1));
+});

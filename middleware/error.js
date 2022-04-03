796a3f41f214f34264881a3712f15526a14e0692
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2022-04-03 16:35:03 +0200

    Handle duplicates and validation errros

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose bad object id
   if (err.name === "CastError") {
     const message = `Resource not found with id ${err.value}.`;
     error = new ErrorReponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field entered.";
+    error = new ErrorReponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorReponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({

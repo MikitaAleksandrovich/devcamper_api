b2543e08f37dcfc95e4cc5b2a1d4af4e57e4d7dd
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2022-07-08 17:00:50 +0200

    Add courses routes to get all courses and specific

---
+++ controllers/courses.js
@@ -0,0 +1,28 @@
+const Course = require("../models/Course");
+const asyncHandler = require("../middleware/asyncErrorhandler");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @description Get all courses
+// @route       GET api/v1/courses
+// @route       GET api/v1/bootcamps/:bootcampId/courses
+// @accesss     Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query;
+
+  // Check if there is a bootcampId in params,
+  // if no then return all Courses
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find();
+  }
+
+  // Executing query
+  const courses = await query;
+
+  res.status(201).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});

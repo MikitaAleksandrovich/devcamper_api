0100b0729229b500bc4992419eb2ee00b56791ca
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2022-07-18 21:22:50 +0200

    Create a user with encrypted password

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,15 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route       GET api/v1/auth/register
 // @accesss     Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(201).json({
-    success: true,
+  const { name, email, password, role } = req.body;
+
+  // Create a user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
   });
+
+  res.status(200).json({ success: true });
 });

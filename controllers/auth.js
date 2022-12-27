b052124b22d4f1fa83daf3a6b9271d232ad5429c
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2022-12-27 15:34:15 +0100

    Set token from cookie and logout and remove token

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,6 +51,21 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponseWithCookie(user, 200, res);
 });
 
+// @description Log user out / clear cookie
+// @route       GET api/v1/auth/logout
+// @accesss     Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @description Get current logged in user
 // @route       POST api/v1/auth/me
 // @accesss     Private
